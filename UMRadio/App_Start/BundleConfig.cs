a26c79550f9a148ed5757223b248824ddd194aa7
peter-mcdonald <pea_mach@hotmail.com>
2015-04-02 16:25:27 +0100

    Getting started

---
+++ UMRadio/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Optimization;
+
+namespace UMRadio.App_Start
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var siteScripts = new ScriptBundle("~/bundles/site");
+            siteScripts.Include("~/Scripts/jquery-2.1.3.js");
+
+            bundles.Add(siteScripts);
+
+            var siteCss = new StyleBundle("~/Content/css");
+            siteCss.Include("~/Content/site.css");
+
+            bundles.Add(siteCss);
+
+#if !DEBUG 
+            BundleTable.EnableOptimizations = true;
+#endif
+        }
+    }
+}
--- UMRadio/Global.asax.cs
+++ UMRadio/Global.asax.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using UMRadio.App_Start;
 
 namespace UMRadio
 {
@@ -19,6 +21,7 @@ namespace UMRadio
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
